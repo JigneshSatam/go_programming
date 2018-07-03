769bb72d2d8c58e567dd550a54aca6a881d20471
Jignesh Satam <jigneshsatam@Jigneshs-MacBook-Pro.local>
2018-07-03 16:10:52 +0530

    Updated concurrency

--- 14_concurrency/01_pipeline_channels/main.go
+++ 14_concurrency/01_pipeline_channels/main.go
@@ -4,36 +4,45 @@ import "fmt"
 
 // Find factorial of a number
 func main() {
-	arr := make([]int64, 66)
-	for i := 1; i <= 66; i++ {
-		arr[i-1] = int64(i)
-	}
-	c := allFacts(arr...)
-	for n := range c {
-		fmt.Println(<-n)
+
+	adderChannel := addNumbers()
+
+	printerChannel := allFacts(adderChannel)
+
+	for n := range printerChannel {
+		fmt.Println(n)
 	}
 }
 
-func allFacts(n ...int64) chan chan int64 {
-	c := make(chan chan int64)
-	go func(n ...int64) {
-		for _, v := range n {
-			c <- fact(v)
-		}
-		close(c)
-	}(n...)
-	return c
-}
-
-func fact(n int64) chan int64 {
+func addNumbers() chan int64 {
 	c := make(chan int64)
-	go func(n int64) {
-		fact := int64(1)
-		for ; n > 0; n-- {
-			fact = fact * n
+	go func() {
+		for j := 1; j <= 200; j++ {
+			for i := 1; i <= 50; i++ {
+				c <- int64(i)
+			}
 		}
-		c <- fact
 		close(c)
-	}(n)
+	}()
 	return c
+}
+
+func allFacts(c chan int64) chan int64 {
+	nc := make(chan int64)
+	go func() {
+		for n := range c {
+			nc <- fact(n)
+		}
+		close(nc)
+	}()
+
+	return nc
+}
+
+func fact(n int64) int64 {
+	fact := int64(1)
+	for ; n > 0; n-- {
+		fact = fact * n
+	}
+	return fact
 }
